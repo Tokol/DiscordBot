fbe39b6941353820f7bb22f9dadf89280520e6f0
sureshlama <suresh.lama@igctech.com.np>
2020-12-25 21:09:32 +0545

    flirt tiny

---
+++ commands/tiny.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'tiny',
+    description: "flirt tiny!",
+    execute(message, args){
+        message.channel.send('Hey tiny, i need a map coz i lost in your eyes!!');
+    }
+}
