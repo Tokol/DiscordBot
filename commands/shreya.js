85e854ba7a123bf6a3f7e8b074a0bf8e11223c1b
sureshlama <suresh.lama@igctech.com.np>
2020-12-26 11:23:18 +0545

    change shreya

--- commands/shreya.js
+++ commands/shreya.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'shreya',
     description: "this is a shreya command!",
     execute(message, args){
-        message.channel.send('Hi I am shreya but you can call me sia pia :butterfly:');
+        message.channel.send('Hi I am sia ghattu ko girlfriend ho ma hehe');
     }
 }
