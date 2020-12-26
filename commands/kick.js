94d9be3ccfbd11a43b0f90661ece3f612c707881
sureshlama <suresh.lama@igctech.com.np>
2020-12-26 15:39:14 +0545

    kick fix

--- commands/kick.js
+++ commands/kick.js
@@ -1,19 +1,14 @@
 module.exports = {
     name: 'kick',
-    description: "this is a kick command!",
-    execute(messages, args){
-       const member = message.mentions.users.first();
-
-       if(member){
-
-        const memberTarger = message.guild.members.cache.get(member.id);
-        memberTarger.kick();
-        message.channel.send("User has been kicked");
-       }
-
-       else{
-           message.channel.send('You couldt kick that member');
-       }
-
+    description: "This command kicks a member!",
+    execute(message, args){
+        const target = message.mentions.users.first();
+        if(target){
+            const memberTarget = message.guild.members.cache.get(target.id);
+            memberTarget.kick();
+            message.channel.send("User has been kicked");
+        }else{
+            message.channel.send(`You coudn't kick that member!`);
+        }
     }
 }
