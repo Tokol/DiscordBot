d482f7ac6990f5c4f19a24f79d75eabeaebf87ef
sureshlama <suresh.lama@igctech.com.np>
2020-12-27 06:56:29 +0545

    ray10

--- commands/ray.js
+++ commands/ray.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ray',
     description: "this is a ray command!",
     execute(message, args){
-        message.channel.send('Bonjur, Ray here. Open for intellectual and business talks.');
+        message.channel.send('Hi, my name is Prashrae Moktan. My friends call me Ray.\nI am a Business Graduate specializing in Marketing and Sales.\nI am curretly based in Sydney. Business and intellectual talks, thats me!!');
     }
 }
