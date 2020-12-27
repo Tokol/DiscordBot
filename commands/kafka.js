cbe6bddc4458470f27b8c0c58c68371c7853cc2e
sureshlama <suresh.lama@igctech.com.np>
2020-12-27 06:45:15 +0545

    batman 8

--- commands/kafka.js
+++ commands/kafka.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'kafka',
     description: "this is a kafka command!",
     execute(message, args){
-        message.channel.send('Hi i am a bot creator, my name is Suresh Lama. \nA Senior Software Developer, a Writer, an Astrologer. \nLoves Literature, Arts and History. \nI am also called as a Silent Guardian, Watchful protector, a Dark Knight, The Batman.');
+        message.channel.send('Hi i am a bot creator, my name is Suresh Lama. \nA Senior Software Developer, a Writer, an Astrologer. \nLoves Literature, Arts and History. \nI am also called as a Silent Guardian, Watchful protector, The Dark Knight, The Batman.');
     }
 }
