a5746923ed7112183d9e72dbe8ab16d28ebd4be8
sureshlama <suresh.lama@igctech.com.np>
2020-12-28 17:54:04 +0545

    lakpa fav

--- commands/fav.js
+++ commands/fav.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'fav',
     description: "this is a fav command!",
     execute(message, args){
-        message.channel.send('fav food - momo \n fav bro - napper \n fav sis - suku');
+        message.channel.send('fav food - momo \n fav bro - napper \n fav sis - suku and lakpa');
     }
 }
