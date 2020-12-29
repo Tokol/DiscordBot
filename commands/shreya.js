d4547e88d9daa09d249b2deceaf750668bae28bb
sureshlama <suresh.lama@igctech.com.np>
2020-12-29 23:03:26 +0545

    shreya change

--- commands/shreya.js
+++ commands/shreya.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'shreya',
     description: "this is a shreya command!",
     execute(message, args){
-        message.channel.send('Hi I am sia ghattu ko girlfriend ho ma hehe');
+        message.channel.send('Hiiiiiii naperrrrrrrr daiiiiiiiiii');
     }
 }
