71a1dd379289875019d7092f76eb221248b42558
sureshlama <suresh.lama@igctech.com.np>
2020-12-30 19:44:04 +0545

    choila update 2

--- commands/choila.js
+++ commands/choila.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'choila',
     description: "this is a choila command!",
     execute(message, args){
-        message.channel.send('Choila khayi najanu hai poila :rofl: yoyo this is tasty choila and\nhere are some things you might want to know about this choila :nerd:\nAge : 20-25\nSex : female/nRelationship status : taken (by some stupid )\nFav food : choila and laphing\nThis is all if you wanna know something you can mail or contact her :wink:');
+        message.channel.send('Choila khayi najanu hai poila :rofl: yoyo this is tasty choila and she is very agressive.\nHowever, she might look like fierce on outside but she is very kind and lovable once you get to know her.\nSo here are some things you might want to know about this choila :nerd:\nAge : 20-25\nSex : female\nHoroscope : aries\nRelationship status : choila is taken by momo :heart:\nFav food : choila and laphing\nThis is all if you wanna know something you can mail or contact her :wink:');
     }
 }
