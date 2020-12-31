461cb712685585034a6085d154612f7b3eb1252c
sureshlama <suresh.lama@igctech.com.np>
2020-12-31 23:14:15 +0545

    new year wish 3

--- commands/newyear.js
+++ commands/newyear.js
@@ -5,7 +5,7 @@ module.exports = {
     execute(message, args){
         const exampleEmbed = new Discord.MessageEmbed()
         .setColor('#00FF00	')
-            .setTitle('Game Invitation')
+            .setTitle('Happy New Year 2021')
             .setDescription(`May this year brings only positive news, except test reports. Happy new year 2021! May you all have a year filled with Love, Laughter, Brightness and Hope.`)
             .setImage('https://i.giphy.com/media/EzK5EPh5Iekd9wwa0m/source.gif')
             .setFooter('Get set ready To inject - Kafka AKA HAL 9000')        
