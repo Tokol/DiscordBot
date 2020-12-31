4cb34077442dbf49132e8acf7cc2e2d7bc410f1e
sureshlama <suresh.lama@igctech.com.np>
2020-12-31 23:22:23 +0545

    new year wish 5

--- commands/newyear.js
+++ commands/newyear.js
@@ -6,7 +6,7 @@ module.exports = {
         const exampleEmbed = new Discord.MessageEmbed()
         .setColor('#00FF00	')
             .setTitle('Happy New Year 2021')
-            .setDescription(`May this year brings only positive news, except test reports. Happy new year 2021! May you all have a year filled with Love, Laughter, Brightness and Hope.`)
+            .setDescription(`May this year brings only positive news, except test reports. May you all have a year filled with Love, Laughter, Brightness and Hope.`)
             .setImage('https://i.giphy.com/media/EzK5EPh5Iekd9wwa0m/source.gif')
             .setFooter('Get set ready To inject - Kafka AKA HAL 9000')        
             message.channel.send(exampleEmbed);
