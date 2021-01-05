775f0ca20360640134d623a478c15d7332e4466e
sureshlama <suresh.lama@igctech.com.np>
2021-01-05 08:24:36 +0545

    fav 3

--- commands/fav.js
+++ commands/fav.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'fav',
     description: "this is a fav command!",
     execute(message, args){
-        message.channel.send('fav food - momo \n fav bro - napper \n fav sis - suku and lakpa \n fav music - Damn miss the year of 2014-2018 era songs. EDM song was on the rise, Artist like Avicii, Chainsmoker, Marshemello and Artist from Norse countries used to top all Billboard and Music chart');
+        message.channel.send('fav food - momo \n fav bro - napper \n fav sis - suku and lakpa \n fav music - Damn miss the year of 2014-2018 era songs. EDM song was on the rise, Artist like Avicii, Chainsmoker, Marshemello and Artist from Norse countries used to top all Billboard and Music chart\n Big fan of EDM and Synth music.');
     }
 }
