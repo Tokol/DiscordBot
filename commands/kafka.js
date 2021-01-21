23b083557e9c3d0b88eccfa6e2ccd5b46d82c5ea
sureshlama <suresh.lama@igctech.com.np>
2021-01-22 00:02:43 +0545

    footer kafka

--- commands/kafka.js
+++ commands/kafka.js
@@ -11,9 +11,7 @@ module.exports = {
         .setDescription('Hi i am a bot creator, my name is Suresh Lama. \nA Senior Software Developer, a Writer, an Astrologer. \nLoves Literature, Arts and History. \nI am also called as a Silent Guardian, Watchful protector, The Dark Knight, The Batman.')
         .setImage('https://i.imgur.com/e1c8E1W.jpg')
         .addField('Come meet me at my Batcave AKA Chautari', `https://discord.gg/c7vFf2cGF2`)
-        
-        .setFooter(`Currently bat-sginal is out of order, just mention me will be there`)  
-        
+        .setFooter(`Currently Bat-Signal is out of order, just mention me will be there`)  
         message.channel.send(embed);
 
        // message.channel.send('');
