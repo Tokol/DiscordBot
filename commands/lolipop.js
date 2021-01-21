18a3c5c23568782f1e98a1eb234a07b88068c384
sureshlama <suresh.lama@igctech.com.np>
2021-01-21 07:40:13 +0545

    lolipop

--- commands/lolipop.js
+++ commands/lolipop.js
@@ -1,9 +1,19 @@
-
+const Discord = require('discord.js');
 module.exports = {
     name: 'lolipop',
     description: "this is a lolipop command",
     execute(message, args){
-        message.channel.send('Hi my name is Lollipop. Come say hi to me in Kuirey ko kafe');
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Lollipop69')
+        .setDescription('Hi my name is Lollipop. Come say hi to me in Kuirey ko kafe ')
+        .addField("join here [link](https://discord.gg/YPtyFe3H7R)")
+        .setURL("https://discord.gg/YPtyFe3H7R")
+        message.channel.send(embed);
+
+
+        //https://discord.gg/YPtyFe3H7R
+       // message.channel.send('Hi my name is Lollipop. Come say hi to me in Kuirey ko kafe');
     }
 }
 
