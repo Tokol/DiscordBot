68b110d4c1292955a8377accbfabc9bf195a9b0a
sureshlama <suresh.lama@igctech.com.np>
2021-01-21 23:48:07 +0545

    kafka change

--- commands/kafka.js
+++ commands/kafka.js
@@ -1,7 +1,18 @@
+const Discord = require('discord.js');
+
 module.exports = {
     name: 'kafka',
     description: "this is a kafka command!",
     execute(message, args){
-        message.channel.send('Hi i am a bot creator, my name is Suresh Lama. \nA Senior Software Developer, a Writer, an Astrologer. \nLoves Literature, Arts and History. \nI am also called as a Silent Guardian, Watchful protector, The Dark Knight, The Batman.');
+
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Kafa AKA HAL 9000 Introduction')
+        .setDescription('Hi i am a bot creator, my name is Suresh Lama. \nA Senior Software Developer, a Writer, an Astrologer. \nLoves Literature, Arts and History. \nI am also called as a Silent Guardian, Watchful protector, The Dark Knight, The Batman.')
+        .setImage('https://i.imgur.com/e1c8E1W.jpg')
+        .addField("come meet me at my Batcave AKA Chautari  https://discord.gg/c7vFf2cGF2")
+        
+        message.channel.send(embed);
+
+       // message.channel.send('');
     }
 }
--- commands/lilo.js
+++ commands/lilo.js
@@ -8,12 +8,8 @@ module.exports = {
         .setTitle('Lilo Intro')
         .setDescription('Hi! My name is Lilo. Find me my Stitch please. <33 ')
         .addField("come meet me here (https://discord.gg/YPtyFe3H7R)")
-            
+        
         message.channel.send(embed);
-
-
-        //https://discord.gg/YPtyFe3H7R
-       // message.channel.send('Hi my name is Lollipop. Come say hi to me in Kuirey ko kafe');
     }
 }
 
