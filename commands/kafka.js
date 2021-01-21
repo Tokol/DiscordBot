2253b1d1eb02ba4e369c28479f68d210adaddfde
sureshlama <suresh.lama@igctech.com.np>
2021-01-21 23:55:43 +0545

    kafka improve3

--- commands/kafka.js
+++ commands/kafka.js
@@ -6,7 +6,7 @@ module.exports = {
     execute(message, args){
 
         const embed = new Discord.MessageEmbed()
-        .setTitle('Kafa AKA HAL 9000 Introduction')
+        .setTitle('KAFKA AKA HAL 9000 INTRODUCTION')
         .setDescription('Hi i am a bot creator, my name is Suresh Lama. \nA Senior Software Developer, a Writer, an Astrologer. \nLoves Literature, Arts and History. \nI am also called as a Silent Guardian, Watchful protector, The Dark Knight, The Batman.')
         .setImage('https://i.imgur.com/e1c8E1W.jpg')
         .addField('Come meet me at my Batcave AKA Chautari', `https://discord.gg/c7vFf2cGF2`)
