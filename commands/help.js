c68756042a5770237649d67d15d1241dd27f7524
sureshlama <suresh.lama@igctech.com.np>
2021-02-16 17:38:49 +0545

    help change2

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports = {
      execute(message, args){
                         var embed = new Discord.MessageEmbed()
                         .setTitle('hal commands')
-                        .setDescription('say "your message" to speak \n coivd or corona (to get covid-19 status)  meme\nslap @mention\nkill @mention\nhug @mention\n kiss @mention\nweather cityname\ncurrency\nfan @mention\ncat\ndog\nrashi zodiac sign\nav @mention\n\n play "music name" \nqueue (show the requested music queue) \nskip (skip next music) \n stop(disconect music) \n loop (reapt the same music)')
+                        .setDescription('say "your message" to speak \n coivd or corona (to get covid-19 status)\nmeme\nslap @mention\nkill @mention\nhug @mention\n kiss @mention\nweather cityname\ncurrency\nfan @mention\ncat\ndog\nrashi zodiac sign\nav @mention\n\n play "music name" \nqueue (show the requested music queue) \nskip (skip next music) \n stop(disconect music) \n loop (reapt the same music)')
                         message.channel.send(embed);
         }
 
