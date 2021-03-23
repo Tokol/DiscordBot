4db4c0189dfe629a8c5ac6273a4bc798a8fd6451
sureshlama <suresh.lama@igctech.com.np>
2021-03-23 18:06:52 +0545

    kafka update

--- commands/kafka.js
+++ commands/kafka.js
@@ -10,7 +10,7 @@ module.exports = {
         .setTitle('Kafa AKA HAL 9000 Introduction')
         .setDescription('Hi i am a bot creator, my name is Suresh Lama. \nA Senior Software Developer, a Writer, an Astrologer. \nLoves Literature, Arts and History. \nI am also called as a Silent Guardian, Watchful protector, The Dark Knight, The Batman.')
         .setImage('https://i.imgur.com/e1c8E1W.jpg')
-        .addField('Come meet me at my Batcave AKA Chautari', `https://discord.gg/hkkMSEYKcu`)
+        .addField('Come meet me at my Batcave AKA Chauki', `https://discord.gg/H6ZepQ6yHR`)
         .setFooter(`Currently Bat-Signal is out of order, just mention me will be there`)  
         message.channel.send(embed);
 
